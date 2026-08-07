8e72529549e1a737572d2c7e52c7a4b11e6ff34b
Zack Hoang <zackhoang2004@gmail.com>
2026-08-07 09:22:25 +0700

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let maxSubStrLen = 0;
+        let l = 0;
+        let set = new Set();
+        for (let r = 0; r < s.length; r++) {
+            if (!set.has(s[r])) {
+                set.add(s[r]);
+            } else {
+                while (set.has(s[r])) {
+                    set.delete(s[l]);
+                    l++;
+                }
+            }
+            set.add(s[r]);
+            maxSubStrLen = Math.max(maxSubStrLen, r - l + 1);
+        }
+        return maxSubStrLen;
+    }
+}
